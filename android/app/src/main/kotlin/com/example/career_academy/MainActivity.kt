b78249cde87274abe69a2beea862bf6cc8a73f06
abbas ali <abbaskhankhan577@gmail.com>
2025-03-20 13:08:34 +0500

    starting the project

---
+++ android/app/src/main/kotlin/com/example/career_academy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.career_academy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/constant/text_style/text_style.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+///***********************************    Small    (10 to 20)         ********************************************************************** */
+
+const style20 = TextStyle(
+  fontSize: 20,
+  color: Colors.black,
+  fontWeight: FontWeight.w700,
+);
+
+const style10 = TextStyle(
+  fontSize: 10,
+  color: Colors.black,
+  fontFamily: 'semibold',
+);
+const style10w = TextStyle(
+  fontSize: 10,
+  color: Colors.white,
+  fontFamily: 'semibold',
+);
+const style10r = TextStyle(
+  fontSize: 10,
+  color: Colors.black,
+  fontFamily: 'regular',
+);
+const style14 = TextStyle(
+  fontSize: 14,
+  color: Colors.black,
+  fontWeight: FontWeight.w300,
+);
+const style17 = TextStyle(
+  fontSize: 17,
+  color: Colors.white,
+  fontWeight: FontWeight.w700,
+);
+const style22 = TextStyle(
+  fontSize: 12,
+  color: Colors.white,
+  fontWeight: FontWeight.w700,
+);
+const style16 = TextStyle(
+  fontSize: 16,
+  color: Colors.white,
+  fontWeight: FontWeight.w700,
+);
+
+///***********************************    Medium  (20 to 25)             ********************************************************************** */
+
+const style25 = TextStyle(
+  fontSize: 25,
+  color: Colors.white,
+  fontWeight: FontWeight.w700,
+);
+const style26 = TextStyle(
+  fontSize: 18,
+  color: Colors.black,
+  fontFamily: 'semibold',
+  fontWeight: FontWeight.w700,
+);
+TextStyle style18regular = TextStyle(
+  fontSize: 35.sp,
+  color: Colors.black,
+  fontFamily: 'regular',
+  fontWeight: FontWeight.w700,
+);
+
+const style16b = TextStyle(
+  fontSize: 16,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+const style14N = TextStyle(
+  fontSize: 16,
+  color: Colors.white,
+  fontWeight: FontWeight.w300,
+);
+const style12 = TextStyle(fontSize: 12, color: Colors.black);
+
+///**********************************    Bold             ********************************************************************** */
+
+const style24B = TextStyle(
+  fontSize: 30,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+const style16B = TextStyle(
+  fontSize: 16,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+const style14B = TextStyle(
+  fontSize: 14,
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+);
+TextStyle style14Brown = TextStyle(
+  fontSize: 12.sp,
+  color: const Color(0xff96979B),
+  fontWeight: FontWeight.bold,
+);
+const style12b = TextStyle(
+  fontSize: 12,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+TextStyle style12bgreen = TextStyle(
+  fontSize: 25.sp,
+  color: const Color(0xff39ad4a),
+  fontWeight: FontWeight.bold,
+);
